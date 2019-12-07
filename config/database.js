f409ecc3745f16aca13231bf13bce94e35f16268
PParkJy <wldus8677@naver.com>
2019-12-07 14:13:18 +0900

    jy_revision

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
 	host : 'localhost',
-	user : 'root',
-	password : '1234',
-	port : 3306,
-	database : 'test'
+	user : 'root', //mysql에 접속하는 계정
+	password : '1234', //계정 비밀번호
+	port : 3306, //mysql 기본 포트
+	database : 'test' //사용할 database
 };
--- server.js
+++ server.js
@@ -1,39 +1,59 @@
-const express = require('express');
-const mysql = require('mysql');
+//서버 실행 -> node server.js
+const express = require('express'); // -> npm install express
+const mysql = require('mysql'); // -> npm install mysql
+//config 폴더의 database.js에서 DB 정보 설정함
 const dbconfig = require('./config/database.js');
 const connection = mysql.createConnection(dbconfig);
 const app = express();
+//express에서 POST 요청을 처리하기 위해서 body-parser 모듈 필요 -> npm install body-parser
 const bodyParser = require('body-parser')
-const ejs = require('ejs')
+const ejs = require('ejs') // -> npm install ejs (했는지 안했는지 기억안남)
 
+//mysql db 연결
 connection.connect();
+
+//--------------걍 설정하길래 설정함-----------------
 app.set('views',__dirname+'/')
 app.set('view engine','ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 app.use(express.static(__dirname+'/'))
+//--------------------------------------------------
 
+//처음 접속되는 / 라우트에 map.html 렌더링
 app.get('/', (req,res) => {
   res.render('map.html')
 })
 
+//map.html에서 작은 사각형 한 칸 누르면 그 중심 위도, 경도와 x,y를 POST 방식으로 얻어오기'
 app.post('/cal_rect', function(req,res){
-	console.log(req.body)
-	var body = req.body
+	console.log(req.body) //req.body -> map.html에서 전송된 정보
+	var body = req.body 
+	//IGNORE -> DB 테이블에 중복된 정보가 존재한다면 지금 넣는 정보 무시
+	// 내 mysql DB 연결 정보는 config폴더의 database.js 참고
+	// 테이블명은 jy_test로 만들었음
+	// center_lat DOUBLE not null
+	// center_lng DOUBLE not null
+	// x INT(11? 사이즈 생각안남) not null
+	// y INT(사이즈 모름) not null
+	//PRIMARY KEY(x,y)
+	//나는 x,y를 primary key로 설정했기 때문에 x,y 비교로 중복 정보 판단 가능
+	connection.query('INSERT IGNORE INTO jy_test (center_lat, center_lng, x, y) VALUES (?,?,?,?)',[body.latitude,body.longitude,body.x,body.y], function(err,rows) {
+                if(err) throw err;
 
-	connection.query('INSERT IGNORE INTO jy_test (center_lat, center_lng, x,y) VALUES (?, ?, ?, ?)',[body.latitude,body.longitude,body.x,body.y] ,function(err,rows) {
-		if(err) throw err;
-		
-		console.log('DB save')
-	})
-	res.json({test:"server ok"})
+		console.log('DB stored'); //DB에 잘 저장됨
+        })
+	res.json({test:"server ok"}) //map.html로 server ok라는 답장 전송
 })
 
+//DB의 모든 정보 확인해보기위해 설정한 /test 라우트
+//192.168.0.39:3000/test로 접속해서 확인
 app.get('/test',(req,res) => {
 	connection.query('SELECT * from jy_test', function(err,rows) {
 		if(err) throw err;
 
+		//rows -> select의 결과
 		console.log('The solution is: ',rows);
 		res.send(rows);
 	})
